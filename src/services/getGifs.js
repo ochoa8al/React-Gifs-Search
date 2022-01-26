76ae2299f1ec5b4471f0915bd4c42e7e5945d8bd
ochoa8al <ochoa8al@gmail.com>
2022-01-26 07:28:06 -0600

    change gifs results of 10 to 12

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 const APIKEY = "WcEkizg1Jttmhl58J58FZcrCoY60BP2l"
 
 export default function getGifs({keywords}={}){
-  const APIURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keywords}&limit=10&offset=0&rating=g&lang=en`
+  const APIURL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keywords}&limit=12&offset=0&rating=g&lang=en`
 
   return fetch(APIURL)
     .then((resp)=> resp.json())
